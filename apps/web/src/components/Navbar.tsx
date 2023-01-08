5a82bad57fb653e04926260d581849f79d030697
Connor Stevens <conmail1224@gmail.com>
2023-01-07 23:44:31 -0500

    Add REST Spec link

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -6,6 +6,7 @@ export default function Navbar() {
     <div className="top-0 w-full h-16 border-b border-gray-700 flex items-center justify-between py-4 px-6">
       <div className="font-semibold">TRPC OpenAPI Demo</div>
       <div className="space-x-3">
+        <Link href={process.env.NEXT_PUBLIC_TRPC_BASE_URL!}>API Spec</Link>
         <Link href="/retrieve">Retrieve</Link>
         <Link href="/create">Create</Link>
         <Link href="/list">List</Link>
